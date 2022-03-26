0d66794b65019272b17f49439d08f76a1fd82c55
Oleksandr Hubanov <alex@0x0h.com>
2022-03-27 00:51:13 +0200

    add Pill Category to DB

---
+++ apteka063_bot/Migrations/20220326221738_AddPillsCategory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apteka063.Migrations
+{
+    public partial class AddPillsCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PillCategory",
+                table: "Pills",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PillCategory",
+                table: "Pills");
+        }
+    }
+}
