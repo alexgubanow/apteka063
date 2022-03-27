18e92fa4fe8f9a2653d5d76d52fcbb7fa9daf1c7
Oleksandr Hubanov <alex@0x0h.com>
2022-03-27 15:22:16 +0300

    implement interactive way to enter token, as well check if it is valid

--- apteka063_bot/Program.cs
+++ apteka063_bot/Program.cs
@@ -16,28 +16,64 @@ public static class Program
     {
         using dbc.Apteka063Context _db = new();
         _db.Database.EnsureCreated();
-        string token = ConfigurationManager.AppSettings["Token"]!;
-        if (token == "")
+        TelegramBotClient Bot = null;
+        var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        if (config == null)
         {
-            Console.WriteLine("Please add your token to the App.config file");
-            return 0;
+            Console.WriteLine("failed to open app config");
+            return -1;
         }
-
-        TelegramBotClient Bot = new(token);
-
-        User me = await Bot.GetMeAsync();
+        //var tokenConfig = config.GetSection("Token");
+        int tryCount = 0;
+        string tokenFromArguments = "";
+        if (args.Length > 0)
+        {
+            tokenFromArguments = args[0];
+        }
+        while ((config!.AppSettings.Settings["Token"] == null || Bot == null) && tryCount < 5)
+        {
+            tryCount++;
+            string token = config!.AppSettings.Settings["Token"].Value ?? "";
+            if (tokenFromArguments != "")
+            {
+                token = tokenFromArguments;
+            }
+            else
+            {
+                Console.WriteLine("Please enter telegram token to be used:");
+                token = Console.ReadLine()!;
+            }
+            try
+            {
+                Bot = new(token);
+                if (config!.AppSettings.Settings["Token"] != null)
+                {
+                    config!.AppSettings.Settings["Token"].Value = token;
+                }
+                else
+                {
+                    config!.AppSettings.Settings.Add(new("Token", token));
+                }
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("Token");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"failed to create bot with given token:\n{token}");
+                Console.WriteLine($"original error message:\n{ex.Message}");
+            }
+        }
+        User me = await Bot!.GetMeAsync();
         Console.Title = me.Username ?? "apteka063";
 
         using var cts = new CancellationTokenSource();
 
         // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
         ReceiverOptions receiverOptions = new() { AllowedUpdates = { } };
-        Bot.StartReceiving(bot.UpdateHandlers.HandleUpdateAsync, bot.UpdateHandlers.HandleErrorAsync, receiverOptions, cts.Token);
+        Bot!.StartReceiving(bot.UpdateHandlers.HandleUpdateAsync, bot.UpdateHandlers.HandleErrorAsync, receiverOptions, cts.Token);
 
         Console.WriteLine($"Start listening for @{me.Username}");
-        while (true)
-        {
-        }
+        Console.ReadKey(true);
         // Send cancellation request to stop bot
         cts.Cancel();
         return 0;
