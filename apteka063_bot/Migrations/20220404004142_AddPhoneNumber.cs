f3d2d1aa92ffd0b238ccedcf91654b6a58001004
Oleksandr Hubanov <alex@0x0h.com>
2022-04-04 04:00:24 +0300

    forgot for phonenumber column added

---
+++ apteka063_bot/Migrations/20220404004142_AddPhoneNumber.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apteka063.Migrations
+{
+    public partial class AddPhoneNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+        }
+    }
+}
